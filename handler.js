d5d085f42cafb413bf92aea5f2e63d0639c7c1d6
maikimarolli <maikimarolli@gmail.com>
2022-03-31 00:10:02 -0300

    Correçao na adiçao de açoes a carteira, e implementação da venda de açoes, tambem mostra todas as operacoes ja efetuadas.

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const {parse} = require("querystring");
+const { parse } = require("querystring");
 
 var url = require('url');
 var path = require('path');
@@ -7,6 +7,7 @@ var path = require('path');
 var listaOperacoes = [];
 var list_acoes = [];
 var listaMinhasAcoes = [];
+var transacoes = 0;
 
 var readFile = (file) => {
     let html = fs.readFileSync(__dirname + "/views/html/" + file, "utf8");
@@ -32,15 +33,15 @@ module.exports = (request, response) => {
         let url_parsed = url.parse(request.url, true);
         switch (url_parsed.pathname) {
             case '/':
-                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.writeHead(200, { 'Content-Type': 'text/html' });
                 response.end(readFile("index.html"));
                 break;
             case '/acoes':
-                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.writeHead(200, { 'Content-Type': 'text/html' });
                 response.end(readFile("acoes.html").replace("{$table}", ''));
                 break;
             case '/operacoes':
-                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.writeHead(200, { 'Content-Type': 'text/html' });
                 let operacoes = readFile("operacoes.html");
 
                 let listaAcoesAbertas = "";
@@ -62,6 +63,7 @@ module.exports = (request, response) => {
                             <td id="tipo">{$tipo}</td>
                             <td id="fracionaria">{$fracionaria}</td>
                             <td id="setor">{$setor}</td>
+                            <td id="estilo">{$estilo}</td>
                             <td>{$valor}</td>
                             <td>{$qtde}</td>
                             <td>{$valorTotal}</td>
@@ -75,6 +77,7 @@ module.exports = (request, response) => {
                         .replace("{$tipo}", a.tipo)
                         .replace("{$fracionaria}", a.fracionaria)
                         .replace("{$setor}", a.setor)
+                        .replace("{$estilo}", a.estilo)
                         .replace("{$valor}", a.valor)
                         .replace("{$qtde}", a.quantidade)
                         .replace("{$valorTotal}", (parseFloat(a.valor) * parseFloat(a.quantidade)));
@@ -110,6 +113,67 @@ module.exports = (request, response) => {
 
                 response.end(operacoes);
                 break;
+            case '/carteira':
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                let carteira = readFile("carteira.html");
+
+                let substituirMinhasAcoes = "";
+                let strAcoesReplace = `
+                <tr>
+                            <td>{$codigo}</td>
+                            <td>{$identificacao}</td>
+                            <td id="tipo">{$tipo}</td>
+                            <td id="fracionaria">{$fracionaria}</td>
+                            <td id="setor">{$setor}</td>
+                            <td>{$valor}</td>
+                            <td>{$qtde}</td>
+                            <td>{$valorTotal}</td>
+                            <td>
+                                <button type="button" onclick="location.href = '/acoes_vender?valor={$acaoTransacao}'" class="btn btn-danger"><i class="fas fa-edit"></i></button>
+                                <button type="button" onclick="location.href = '/operacoes'" class="btn btn-success"><i class="far fa-trash-alt"></i></button>
+                            </td>
+                        </tr>
+                `;
+
+                listaMinhasAcoes.forEach((a) => {
+                    substituirMinhasAcoes += strAcoesReplace
+                        .replace("{$codigo}", a.codigo)
+                        .replace("{$identificacao}", a.identificacao)
+                        .replace("{$tipo}", a.tipo)
+                        .replace("{$fracionaria}", a.fracionaria)
+                        .replace("{$setor}", a.setor)
+                        .replace("{$valor}", a.valor)
+                        .replace("{$qtde}", a.quantidade)
+                        .replace("{$valorTotal}", (parseFloat(a.valor) * parseFloat(a.quantidade)))
+                        .replace("{$acaoTransacao}", a.transacaoId);
+                });
+
+                carteira = carteira.replace("{$listaMinhasAcoes}", substituirMinhasAcoes);
+
+                response.end(carteira);
+                break;
+
+            case "/acoes_vender":
+                response.writeHead(200, { "Content-Type": "text/html" });
+                var str = request.url.split("valor=");
+                let aux;
+                for (let i = 0; i < listaMinhasAcoes.length; i++) {
+                    if (listaMinhasAcoes[i].transacaoId === parseInt(decodeURI(str[1]))) {
+
+                        aux = JSON.parse(JSON.stringify(listaMinhasAcoes[i]));
+
+                        listaMinhasAcoes = listaMinhasAcoes.filter((v) => {
+                            return !(listaMinhasAcoes[i].transacaoId === v.transacaoId);
+                        });
+
+                    }
+                }
+                aux.transacaoId = transacoes;
+                transacoes++;
+                aux.estilo = 'V';
+                listaOperacoes.push(aux);
+                response.end(readFile('index.html'));
+                break;
             default:
                 break;
         }
@@ -117,7 +181,7 @@ module.exports = (request, response) => {
         switch (request.url.trim()) {
             case '/submitAcoes':
                 collectData(request, 'acoes', () => {
-                    response.writeHead(200, {'Content-Type': 'text/html'});
+                    response.writeHead(200, { 'Content-Type': 'text/html' });
                     let html_retorno = '';
                     for (let dados of list_acoes) {
                         let fracionaria = 'Não';
@@ -154,20 +218,24 @@ module.exports = (request, response) => {
                 break;
             case "/acao_comprar":
                 collectData(request, 'operacoes', (data) => {
-                    response.writeHead(200, {"Content-Type": "text/html"});
+                    response.writeHead(200, { "Content-Type": "text/html" });
+                    let aux;
                     list_acoes.forEach((a) => {
-                        if(a.codigo == data.acao)
-                        {
-                            a.quantidade = data.quantidade;
-                            listaOperacoes.push(a);
-                            listaMinhasAcoes.push(a);
+                        if (a.codigo == data.acao) {
+                            aux = JSON.parse(JSON.stringify(a));
+                            aux.transacaoId = transacoes;
+                            transacoes++;
+                            aux.quantidade = data.quantidade;
+                            aux.estilo = 'C'
+                            listaOperacoes.push(aux);
+                            listaMinhasAcoes.push(aux);
                         }
                     })
                     response.end(readFile("index.html"));
                 });
                 break;
             default:
-                response.writeHead(404, {'Content-Type': 'text/plain'});
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
                 response.end('Not a post action!');
                 break;
         }
